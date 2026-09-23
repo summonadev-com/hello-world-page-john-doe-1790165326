1dbe5818b13426a59e66c361f61c740e5ed02d8d
John Doe <john123doe.dev@yopmail.com>
2026-09-23 12:21:41 +0000

    make home page a blank white page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,11 +5,5 @@ export const Route = createFileRoute('/')({
 });
 
 function HomePage() {
-  return (
-    <div className="flex min-h-screen items-center justify-center px-6">
-      <h1 className="text-5xl font-semibold tracking-tight text-slate-900 sm:text-6xl">
-        Hello World
-      </h1>
-    </div>
-  );
+  return <div className="min-h-screen bg-white" />;
 }
